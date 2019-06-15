50da3c3861abd788027a948ad79caccbe882ce2e
Dillon Dixon <dillondixon@gmail.com>
2019-06-15 11:18:38 -0700

    hook for wayland firefox

--- systems/personal-laptop/link.sh
+++ systems/personal-laptop/link.sh
@@ -14,5 +14,6 @@ link ./sway/py3status.conf ~/.config/sway/py3status.conf
 link ./sway/background.png ~/.config/sway/background.png
 link ./terminator.conf ~/.config/terminator/config
 link ./keychain.sh ~/.zshrc-extras/keychain.sh
+link ./firefox_wayland.sh ~/.zshrc-extras/firefox_wayland.sh
 link ./env-scripts ~/.env-scripts
 link ./env-scripts-hook.sh ~/.zshrc-extras/env-scripts-hook.sh
