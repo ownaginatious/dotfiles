2b100b0c427c9779eec958db0dc242635d8e19ec
Dillon Dixon <git@dillon.io>
2021-04-10 23:33:18 -0700

    remove egpu specific settings

--- systems/personal-laptop/link.sh
+++ systems/personal-laptop/link.sh
@@ -10,7 +10,6 @@ mkdir -p ~/.config/{sway,terminator}/
 
 link ./sway/config ~/.config/sway/config
 link ./sway/py3status.conf ~/.config/sway/py3status.conf
-link ./sway/background.png ~/.config/sway/background.png
 link ./terminator.conf ~/.config/terminator/config
 link ./keychain.sh ~/.zshrc-extras/keychain.sh
 link ./firefox_wayland.sh ~/.zshrc-extras/firefox_wayland.sh
