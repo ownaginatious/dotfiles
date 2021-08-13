2d4d234b691e18576e35747fa85c8b81423ac787
Dillon Dixon <git@dillon.io>
2021-08-12 23:10:33 -0700

    fix script linking

--- systems/personal-laptop/link.sh
+++ systems/personal-laptop/link.sh
@@ -16,5 +16,5 @@ link ../_shared/mako.conf ~/.config/mako/config
 link ./env ~/.env/personal-laptop
 
 for f in ./scripts/*; do
-  link "./scripts/${f}" "~/.scripts/${f}"
+  link "${f}" "~/.scripts/${f#./scripts/}"
 done
