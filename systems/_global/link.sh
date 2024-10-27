c05f51eb33cf92b1d2b7b39b3dfa3ce4a124ee6f
Dillon Dixon <git@dillon.io>
2024-10-27 15:13:49 -0700

    remove gitconfig files from repo

--- systems/_global/link.sh
+++ systems/_global/link.sh
@@ -3,8 +3,6 @@
 source ../../link.sh
 
 link ./gitignore ~/.gitignore
-link ./mux-gitconfig ~/.gitconfig
-link ./personal-gitconfig ~/git/personal/.gitconfig
 link ./nanorc ~/.nanorc
 link ./tmux.conf ~/.tmux.conf
 link ./vimrc ~/.vimrc
